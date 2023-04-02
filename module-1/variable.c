4bf3b911594cdc2c9cb81be1fca421d179b9fd83
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-02 12:36:00 +0600

    variable added

---
+++ module-1/variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int rohim;
+    rohim = 10;
+    float korim = 5.52;
+    char kamal = 'k';
+
+    printf("%d %0.2f %c", rohim, korim, kamal);
+    return 0;
+}
