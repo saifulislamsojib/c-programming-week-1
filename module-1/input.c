fd090f61d3e8acbe1402eab621c23734d62400aa
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-02 13:36:38 +0600

    input added

---
+++ module-1/input.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a, b, c;
+    // scanf("%d %d %d", &a, &b, &c);
+    // printf("%d %d %d", a, b, c);
+
+    // int a;
+    // float b;
+    // char c;
+    // scanf("%d %f %c", &a, &b, &c);
+    // printf("%d %0.2f %c", a, b, c);
+
+    // int a, b, c;
+    // char p;
+    // scanf("%d%c %d%c %d%c", &a, &p, &b, &p, &c, &p);
+    // printf("%d%% %d%% %d%%", a, b, c);
+
+    int a, b, c;
+    scanf("%d%% %d%% %d%%", &a, &b, &c);
+    printf("%d%% %d%% %d%%", a, b, c);
+    // scanf("%da %db %dc", &a, &b, &c);
+    // printf("%da %db %dc", a, b, c);
+    return 0;
+}
