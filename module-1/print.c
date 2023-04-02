1ea02c73ce53497c90d080dc57e77afc8b3d4d42
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-02 12:09:12 +0600

    print added

---
+++ module-1/print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("\\");
+    // printf("%%");
+    // printf("Hello World\n");
+    // printf("Ami Sojib.\n");
+    // printf("New line a jaw");
+    printf("Hello World\nAmi Sojib.\nNew line a jaw");
+    return 0;
+}
+
+// \n - new line
+// \t - tab
+// % - %%
+// \ - \\
