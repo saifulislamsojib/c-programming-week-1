c94640a1955977071c1812345b62b23a32df99ea
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-03 02:03:36 +0600

    condition added

---
+++ module-2/condition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk >= 100)
+    {
+        printf("Ami burger khabo.");
+    }
+    else
+    {
+        printf("Ami saradin kisu khabo na.");
+    }
+    return 0;
+}
