7bd0b73e3457e15a336cd03017d5ff99a4c43bdf
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-03 01:51:11 +0600

    arithmetic added

---
+++ module-2/arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 15, b = 4;
+    // int c = a + b;
+    // int c = b - a;
+    // int c = a * b;
+    // float c = (a * 1.0) / b;
+    int c = a % b;
+    printf("%d", c);
+    return 0;
+}
