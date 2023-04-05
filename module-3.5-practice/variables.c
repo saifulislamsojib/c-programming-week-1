a95d8bef4224cdbb4f2a04aa77067d5743e3ed73
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-06 00:59:18 +0600

    module 3.5 practice added

---
+++ module-3.5-practice/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    long long int b;
+    float c;
+    char d;
+    scanf("%d %lld %f %c", &a, &b, &c, &d);
+    printf("%d\n%lld\n%0.2f\n%c\n", a, b, c, d);
+    return 0;
+}
